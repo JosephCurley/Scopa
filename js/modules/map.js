a40d3be4532e772da26517289dc66cd536aabef2
Soyburgers <hey.josephcurley@gmail.com>
2015-05-02 18:20:50 -0400

    Map updates. CSS fixes
    fixed css paths. Updated Index page

--- js/modules/map.js
+++ js/modules/map.js
@@ -3,15 +3,28 @@ jQuery(document).ready(function($) {
 	var map;
 	var mapContainer = $('.map-module')[0];
 	function mapInitialize() {
+    var latLng =  new google.maps.LatLng(42.364176, -71.054160);
+
   	var mapOptions = {
     	zoom: 18,
-    	center: new google.maps.LatLng(42.3649, -71.0551)
+      zoomControl: false,
+    	center: latLng
   	};
   	map = new google.maps.Map(mapContainer,
       mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      title:"Scopa"
+    });
+
+    marker.setMap(map);
+
 	}
 
 
 
 	mapInitialize();
+
+
 });
